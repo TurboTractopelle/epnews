24a5dfd811cd807c1d3ba56c58311192c9ce9975
Guillaume Drouen <gdrouen@siege.edpsciences.net>
2019-08-27 11:58:37 +0200

    fix: imgProcess error throw

--- src/hlItem/hlItem.js
+++ src/hlItem/hlItem.js
@@ -2,7 +2,8 @@ const chalk = require("chalk");
 const imgProcess = require("./imgProcess");
 const docxProcess = require("./docxProcess");
 
-async function hl(hlItem, vol, issue) {
+function hl(hlItem, vol, issue) {
+	// @ts-ignore
 	console.log(chalk`{cyan Processing ${hlItem.author} HL}`);
 	return Promise.all([imgProcess(hlItem), docxProcess(hlItem, vol, issue)]);
 }
--- src/hlItem/imgProcess.js
+++ src/hlItem/imgProcess.js
@@ -11,8 +11,11 @@ async function imgProcess(imgData) {
 			const dstPath = path.join(__dirname, `../../out/${imgData.author}.jpg`);
 
 			await sharp(srcPath).toFile(dstPath, err => {
-				return new Error(err);
+				if (err) {
+					throw new Error("Error processing file");
+				}
 			});
+			// @ts-ignore
 			console.log(chalk`{green Processing ${imgData.author}.jpg}`);
 		} catch (err) {
 			return new Error(err);
